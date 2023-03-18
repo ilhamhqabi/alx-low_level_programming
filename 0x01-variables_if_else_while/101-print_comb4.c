ad6932c4202cecee1493cba76632962b8b998197
ilham hqabi <ilham.hqabi@gmail.com>
2023-03-18 16:34:24 -0700

    success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 
 /**
-* main - main function
+* main - print three number combination
 *
-* Return: Always 0
+* Return : 0 
 */
 int main(void)
 {
-int i;
-int j;
-int k;
-for (i = 0 ; i < 10 ; i++)
+int i = '0';
+int j = '0';
+int k = '0';
+while (i <= '7')
 {
-for (j = 1 ; j < 10 ; j++)
+while (j <= '8')
 {
-for (k = 2 ; k < 10 ; k++)
+while (k <= '9')
 {
 if (i < j && j < k)
 {
-putchar(i = '0');
-putchar(j = '0');
-putchar(k = '0');
-if (i +j + k != 24)
-{	
+putchar(i);
+putchar(j);
+putchar(k);
+if (!(i == '7' && j == '8' && k == '9'))
+{
 putchar(',');
 putchar(' ');
 }
 }
+k++;
 }
+k = '0';
+j++;
 }
+j = '0';
+i++;
 }
 putchar('\n');
 return (0);
